47148ce2e21fdc3f2e072df40f610890d34d4a44
Gavan Browne <gavan.browne@gmail.com>
2026-03-11 15:14:24 +0000

    style: update styles for add item form
    Removed red borders used in development. Added cool button style for
    submit button.

--- src/components/AddItemForm.tsx
+++ src/components/AddItemForm.tsx
@@ -20,7 +20,7 @@ const handleSubmit = (e: React.FormEvent) =>{
         <header> <h2>Add Shopping List Item</h2> <button onClick={onClose} type="button">❌</button></header>
        <div className="input-container">
         <div className="form-group">
-<label htmlFor="add-item">New Shopping Item</label>
+<label htmlFor="add-item">New Item</label>
        <input type="text" value={name} onChange={(e)=>setName(e.target.value)} name="add-item" id="add-item" />
         </div>
         
@@ -38,28 +38,25 @@ export default AddItemForm
 const Wrapper = styled.form`
 position: absolute;
 height: 30vh;
-width: 80%;
+width: 82%;
 top: 50%;
 left: 50%;
 transform: translate(-50%, -50%);
 background-color: peachpuff;
 border-radius: 10px;
 header{
-    position: relative;
+    height: 3rem;
     display: flex;
-    border: red solid 1px;
     justify-content: center;
     background-color: #fdbd85;
      border-top-right-radius: 10px;
     border-top-left-radius: 10px;
     align-items: center;
     h2{
-        left: 50%;
-        transform: translate(-50%);
     height: 2rem;
     text-align: center;
     line-height: 2rem;
-    position: absolute;
+   
 }
  button{
         margin-right: 1rem;
@@ -67,20 +64,22 @@ header{
         border: none;
         background: none;
         cursor: pointer;
+        position: absolute;
+        right: .5rem;
     }
 }
   .input-container{
     display: grid;
-    gap: 1rem;
+    gap: 2rem;
     place-content: center;
      text-align: center;
         width: 80%;
         height: calc(100% - 2rem);
-        border: red solid 1px;
         margin: auto;
         label{
             line-height: 2rem;
-              margin-right: .5rem;
+            font-size: .9rem;
+            margin-right: .5rem;
         }
         input{    
          height: 2rem;
@@ -90,5 +89,27 @@ header{
             height: 2rem;
             align-items: center;
         }
+         button{
+       padding: 17px 40px;
+  border-radius: 50px;
+  cursor: pointer;
+  border: 0;
+  background-color: white;
+  box-shadow: rgb(0 0 0 / 5%) 0 0 8px;
+  letter-spacing: 1.5px;
+  text-transform: uppercase;
+  font-size: 15px;
+  transition: all 0.5s ease;
     }
+    button:active {
+  letter-spacing: 3px;
+  background-color: hsl(261deg 80% 48%);
+  color: hsl(0, 0%, 100%);
+  box-shadow: rgb(93 24 220) 0px 0px 0px 0px;
+  transform: translateY(10px);
+  transition: 100ms;
+}
+
+    }
+   
 `;

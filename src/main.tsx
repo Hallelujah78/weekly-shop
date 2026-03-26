5698a70f932ca5d77a2828ac078323e51d71ccd3
Gavan Browne <gavan.browne@gmail.com>
2026-03-26 18:01:27 +0000

    feat: added toast container to main.tsx with props
    Imported and added toast container to main.tsx. We can use this to notify
    the user when they successfully copy their shopping list to the clipboard.

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 import GlobalStyle from "./styles/GlobalStyles.ts";
+import { ToastContainer, Bounce } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GlobalStyle />
+    <ToastContainer
+    position="top-right"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick={false}
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="light"
+    transition={Bounce}
+    />
     <App />
   </React.StrictMode>
 );

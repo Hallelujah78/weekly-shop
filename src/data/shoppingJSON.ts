c25862a62f0be63013b2e59fb4f38a39105706a2
Gavan Browne <gavan.browne@gmail.com>
2026-03-28 18:26:01 +0000

    refactor: add shoppingJSON.ts for more Firebase-friendly data
    Added shoppingJSON.ts - reformat shopping.ts to be a JSON object where
    the item UUID IDs are now the keys for each item.

---
+++ src/data/shoppingJSON.ts
@@ -0,0 +1,189 @@
+
+export const shoppingList = {
+  "shoppingList": {
+    "3280f8fe-30b2-4531-929f-a74b6039d82d": {
+      "name": "Pasta Ready Meal",
+      "strike": false
+    },
+    "ec0df968-47f0-4924-99b7-577c38d1455a": {
+      "name": "Pizza",
+      "strike": false
+    },
+    "252c342b-a1c0-4928-9cc7-17ec1b32d1c7": {
+      "name": "Decaf Coffee",
+      "strike": false
+    },
+    "3923a5cd-a6e0-4c77-9461-619862010564": {
+      "name": "Pasta Sauce",
+      "strike": false
+    },
+    "aa3bf886-c5e3-405d-b013-58ea6db79f45": {
+      "name": "Salt",
+      "strike": false
+    },
+    "873113a7-385d-4bb4-a5ef-3cc335a0f14c": {
+      "name": "Clementines",
+      "strike": false
+    },
+    "bd1a8c73-6d9a-48f8-93c7-2b82d950d785": {
+      "name": "Cherry Cake",
+      "strike": false
+    },
+    "de74727f-cd6a-4991-998d-06ca61fd3d7f": {
+      "name": "Honey",
+      "strike": false
+    },
+    "d44fcf01-cb7a-46d2-9e62-d582f744e040": {
+      "name": "Actimel",
+      "strike": false
+    },
+    "f78f7699-2da7-4983-9bf0-2351e0b0a015": {
+      "name": "Sugar",
+      "strike": false
+    },
+    "1bdf3d4f-3dfd-4796-ae57-d213e0e6096b": {
+      "name": "Wine",
+      "strike": false
+    },
+    "21a7a9e7-85db-45ce-869c-0ca437e820db": {
+      "name": "Toilet Roll",
+      "strike": false
+    },
+    "2c61afdb-1273-44f8-9848-43b7a937a2a5": {
+      "name": "Toothpaste",
+      "strike": false
+    },
+    "9857e7e9-c822-492c-ac7b-84e1b3d0e859": {
+      "name": "Toothbrush",
+      "strike": false
+    },
+    "c5da3314-4914-41a4-b2b5-3338419e0064": {
+      "name": "Chicken Breasts",
+      "strike": false
+    },
+    "d609b4b2-a9dd-4e1b-9e5a-bfa3581040f7": {
+      "name": "Mince",
+      "strike": false
+    },
+    "a9856965-a459-4b5f-97f2-c4582ef66e9c": {
+      "name": "D Pearl Vitamin D Supplements",
+      "strike": false
+    },
+    "6f129b5b-ec5c-4d92-a3ca-13e27586bd82": {
+      "name": "Nutricream",
+      "strike": false
+    },
+    "2fa6116f-4f10-4ca4-9f78-3fb5b8a4b8c4": {
+      "name": "Ketchup",
+      "strike": false
+    },
+    "b42455be-08cf-4611-90be-5154a0c2c628": {
+      "name": "Porridge",
+      "strike": false
+    },
+    "a7bcede0-e6c3-483a-a211-ee283fdc033d": {
+      "name": "Mini Magnums",
+      "strike": false
+    },
+    "b4ac7e4c-f7dd-4120-a7f4-854605582614": {
+      "name": "Tea Caffinated",
+      "strike": false
+    },
+    "bfd2d461-94a8-4061-9605-1158300d2227": {
+      "name": "Kitchen Roll",
+      "strike": false
+    },
+    "baf21044-2925-4e8d-a606-193558c2f4e5": {
+      "name": "Tissue",
+      "strike": false
+    },
+    "4b28b3c9-3fed-45a5-9be2-908371d99812": {
+      "name": "Cigarette Filters",
+      "strike": false
+    },
+    "ee412b7b-87ad-439d-9d15-dc138f322d50": {
+      "name": "Hair Colouring",
+      "strike": false
+    },
+    "a8fede64-4023-4442-bd9d-f043cba35925": {
+      "name": "Dogfood",
+      "strike": false
+    },
+    "de092829-d122-4355-af0d-2fdc0f80d912": {
+      "name": "Bananas",
+      "strike": false
+    },
+    "fba36e2c-0830-4b54-8d7f-5a612519f776": {
+      "name": "Bread",
+      "strike": false
+    },
+    "44fbb33d-c14a-48f6-a271-d6f48b7672ae": {
+      "name": "Potatoes",
+      "strike": false
+    },
+    "c6da29f9-e10d-4930-b8dc-62452460ae8b": {
+      "name": "Milk",
+      "strike": false
+    },
+    "d6a3d902-7787-499c-8126-4fd67ef86bd0": {
+      "name": "Rice",
+      "strike": false
+    },
+    "f5775f63-e625-4d07-8c62-54ce9316b3b9": {
+      "name": "Beans",
+      "strike": false
+    },
+    "1d5cf918-b46c-48a3-8be4-2c84109db09f": {
+      "name": "Eggs",
+      "strike": false
+    },
+    "9dabd594-e4ca-4aa7-aa7a-110388accf81": {
+      "name": "Activia Strawberry",
+      "strike": false
+    },
+    "af01b66c-218d-4cf3-985d-198958b6ad98": {
+      "name": "Peach Activia",
+      "strike": false
+    },
+    "33df3bd8-3d25-403a-9ba1-5a032eb71ac3": {
+      "name": "Benecol",
+      "strike": false
+    },
+    "deb46250-c724-4ab0-90f7-4193801e947b": {
+      "name": "Firelighters",
+      "strike": false
+    },
+    "55ace688-541c-4309-951f-61bea0480898": {
+      "name": "Ham",
+      "strike": false
+    },
+    "3d73741f-cc7c-4cfa-a625-27586a36819b": {
+      "name": "Cheddar Cheese Slices",
+      "strike": false
+    },
+    "842f6f36-fdf5-4922-aad8-21de78af7c49": {
+      "name": "Maryland Cookies",
+      "strike": false
+    },
+    "aca90082-88d0-4a14-a930-f7096d565a1c": {
+      "name": "Carrots",
+      "strike": false
+    },
+    "573622cf-b18f-4377-a1ca-683490514d13": {
+      "name": "Fish Fingers",
+      "strike": false
+    },
+    "1e0c55ca-75a3-47dc-9a41-eef0b7b63785": {
+      "name": "Lucozade",
+      "strike": false
+    },
+    "e30e4305-e64e-4855-a53d-d96e3392b4cb": {
+      "name": "Decaf Tea",
+      "strike": false
+    },
+    "a77ca39f-bf4a-4a80-a6b5-6bb1b1e4a674": {
+      "name": "Coffee",
+      "strike": false
+    }
+  }
+}
